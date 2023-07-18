c81d5554d6c2edbdc9cd15bc638fe872a93a1f33
kimhw7 <khw786678@gmail.com>
2023-07-18 22:16:09 +0900

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Footer from "./components/footer";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -19,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,13 @@
+import { styled } from "styled-components";
+
+const Footer = () => {
+  return <FooterContainer></FooterContainer>;
+};
+
+const FooterContainer = styled.footer`
+  background-color: pink;
+  width: 100vw;
+  height: 10vh;
+`;
+
+export default Footer;
