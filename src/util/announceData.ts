af1751fbc5bee99b5cf70d24a2f7916598e5f517
kimhw <khw786678@gmail.com>
2023-07-19 17:25:09 +0900

    회사연혁 데이터 입력

--- src/util/announceData.ts
+++ src/util/announceData.ts
@@ -4,7 +4,25 @@ type AnnounceDataType = {
 }[];
 
 const AnnounceData: AnnounceDataType = [
-  { date: "2023-07-10", content: "이사했음" },
+  {
+    date: "2023-05",
+    content: "가산역 반도아이비밸리 지식산업센터로 본사 이전",
+  },
+  {
+    date: "2022-01",
+    content:
+      "M365 헬프데스크 상주 인력파견 및 비상주 헬프데스크 운영 (O365, TEAMS, INTUNE 등 통합관리)",
+  },
+  { date: "2021-04", content: "메일시스템 통합 외주 HelpDesk 운영 시작" },
+  { date: "2017-06", content: "(주)선유씨엔에스 인수. (동종업무 법인)" },
+  { date: "2017-01", content: "Hyper-V기반 Windows 가상서버 호스팅 사업 개시" },
+  { date: "2015-04", content: "사당동으로 본사 이전" },
+  { date: "2014-08", content: "개발부서 설립" },
+  { date: "2010-05", content: "MicroSoft업무 Project 협업 시작" },
+  { date: "2007-08", content: "해외 서버 관리서비스 수행 시작" },
+  { date: "2007-01", content: "㈜인포필드 법인 전환" },
+  { date: "2003-07", content: "Exchange Server 업무 수행 시작" },
+  { date: "2002-02", content: "개인업체 창업" },
 ];
 
 export default AnnounceData;
