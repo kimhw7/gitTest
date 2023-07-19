3d6e11cdd95b476364366b5f6416747be8362b8e
kimhw <khw786678@gmail.com>
2023-07-19 10:07:41 +0900

    기본 레이아웃

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
+import { styled } from "styled-components";
+import { Reset } from "styled-reset";
+
 import Footer from "./components/footer";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Reset />
+      <DefaultWrapper className="background">
+        <div className="contentWrapper">{/* content */}</div>
+
+        <Footer />
+      </DefaultWrapper>
+    </>
   );
 }
+
+const DefaultWrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: 000000;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
 
 export default App;
