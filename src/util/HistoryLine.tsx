99331cf199fd98f80ef55ef0b3dbba01a3158614
kimhw <khw786678@gmail.com>
2023-07-20 16:36:46 +0900

    중간저장

---
+++ src/util/HistoryLine.tsx
@@ -0,0 +1,14 @@
+import { styled } from "styled-components";
+
+type Props = {
+  date: string;
+  content: string;
+};
+
+const HistoryLine = (props: Props) => {
+  return <div></div>;
+};
+
+const HistoryLineWrapper = styled.div``;
+
+export default HistoryLine;
